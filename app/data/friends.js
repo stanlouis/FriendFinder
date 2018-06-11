5111f73d6d2afbf06ffaa9e23a9527cfb150b711
Stanley Louis <stanlouis@msn.com>
2018-06-11 09:47:34 -0400

    Add modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,112 +1,417 @@
-let friends = [{
-  "name": "Ahmed",
-  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-  "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
-}, {
-  "name": "Jacob Deming",
-  "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
-  "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
-}, {
-  "name": "Jeremiah Scanlon",
-  "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
-  "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
-}, {
-  "name": "Louis T. Delia",
-  "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-  "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
-}, {
-  "name": "Lou Ritter",
-  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
-  "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
-}, {
-  "name": "Jordan Biason",
-  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
-  "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
-}, {
-  "name": "GoGiphy",
-  "photo": "/k",
-  "scores": ["3", "2", "3", "3", "3", "3", "3", "3", "2", "2"]
-}, {
-  "name": "jhvj",
-  "photo": "bkibik",
-  "scores": ["5", "3", "2", "4", "2", "5", "3", "3", "3", "5"]
-}, {
-  "name": "Christina",
-  "photo": "https://goo.gl/images/2XqE2z",
-  "scores": ["5", "3", "2", "2", "3", "2", "3", "2", "2", "2"]
-}, {
-  "name": "Christina",
-  "photo": "https://goo.gl/images/2XqE2z",
-  "scores": ["5", "1", "4", "4", "3", "2", "3", "2", "2", "2"]
-}, {
-  "name": "Christina",
-  "photo": "https://goo.gl/images/2XqE2z",
-  "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "1", "2"]
-}, {
-  "name": "sadf",
-  "photo": "sadf",
-  "scores": ["2", "4", "3", "4", "2", "3", "3", "3", "4", "3"]
-},  {
-  "name": "Dan",
-  "photo": "https://www.google.com/search?q=man&safe=active&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjggtTYwsfbAhVGy1QKHY8KBGMQ_AUICigB&biw=1230&bih=573#imgrc=E_KQXHj3lJpuwM:",
-  "scores": ["2", "3", "3", "3", "2", "4", "3", "2", "3", "3"]
-}, {
-  "name": "Dan",
-  "photo": "https://www.google.com/search?q=man&safe=active&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjggtTYwsfbAhVGy1QKHY8KBGMQ_AUICigB&biw=1230&bih=573#imgrc=E_KQXHj3lJpuwM:",
-  "scores": ["2", "3", "3", "3", "2", "4", "3", "2", "3", "3"]
-}, {
-  "name": "Dan",
-  "photo": "https://www.google.com/search?q=man&safe=active&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjggtTYwsfbAhVGy1QKHY8KBGMQ_AUICigB&biw=1230&bih=573#imgrc=E_KQXHj3lJpuwM:",
-  "scores": ["2", "3", "3", "3", "2", "4", "3", "2", "3", "3"]
-}, {
-  "name": "Mark Diong",
-  "photo": "DF",
-  "scores": ["1", "1", "2", "1", "1", "3", "3", "3", "3", "3"]
-}, {
-  "name": "J",
-  "photo": "isdf",
-  "scores": ["2", "2", "2", "1", "3", "3", "2", "3", "4", "1"]
-}, {
-  "name": "dan",
-  "photo": "https://www.google.com",
-  "scores": ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
-}, {
-  "name": "Smithy",
-  "photo": "Http://photo.com/photo.jpg",
-  "scores": ["1", "1", "1", "1", "2", "1", "2", "5", "4", "2"]
-}, {
-  "name": "dan",
-  "photo": "https://www.google.com",
-  "scores": ["2", "2", "2", "3", "2", "3", "3", "3", "1", "1"]
-},
+let friends = [
   {
-  "name": "Mike",
-  "photo": "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg",
-  "scores": ["4", "1", "2", "4", "3", "2", "3", "1", "3", "2"]
-}, {
-  "name": "Mike",
-  "photo": "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg",
-  "scores": ["4", "1", "2", "4", "3", "2", "3", "1", "3", "2"]
-}, {
-  "name": "Mike",
-  "photo": "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg",
-  "scores": ["4", "1", "2", "4", "3", "2", "3", "1", "3", "2"]
-}, {
-  "name": "Mike",
-  "photo": "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg",
-  "scores": ["4", "1", "2", "4", "3", "2", "3", "1", "3", "2"]
-}, {
-  "name": "Mark Diong",
-  "photo": "DF",
-  "scores": ["1", "1", "2", "1", "1", "3", "3", "3", "3", "3"]
-}, {
-  "name": "Stanley Louis",
-  "photo": "https://cdn.pixabay.com/photo/2018/06/08/23/35/black-panther-3463377_1280.jpg",
-  "scores": ["5", "5", "5", "5", "2", "3", "3", "3", "3", "4"]
-}, {
-  "name": "Mike",
-  "photo": "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg",
-  "scores": ["4", "1", "2", "4", "3", "2", "3", "1", "3", "2"]
-}];
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ" +
+        "4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  }, {
+    name: "Jacob Deming",
+    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  }, {
+    name: "Jeremiah Scanlon",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  }, {
+    name: "Louis T. Delia",
+    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  }, {
+    name: "Lou Ritter",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk" +
+        "2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  }, {
+    name: "Jordan Biason",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA" +
+        "0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }, {
+    name: "Wonder Woman",
+    photo: "https://cdn.pixabay.com/photo/2017/06/07/19/30/wonder-woman-2381272_1280.jpg",
+    scores: [
+      "3",
+      "2",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "2",
+      "2"
+    ]
+  }, {
+    name: "Spider Man",
+    photo: "https://cdn.pixabay.com/photo/2016/08/08/20/44/spiderman-1579249_1280.jpg",
+    scores: [
+      "5",
+      "3",
+      "2",
+      "4",
+      "2",
+      "5",
+      "3",
+      "3",
+      "3",
+      "5"
+    ]
+  }, {
+    name: "Christina",
+    photo: "https://goo.gl/images/2XqE2z",
+    scores: [
+      "5",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "3",
+      "2",
+      "2",
+      "2"
+    ]
+  }, {
+    name: "Christina",
+    photo: "https://goo.gl/images/2XqE2z",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "3",
+      "2",
+      "3",
+      "2",
+      "2",
+      "2"
+    ]
+  }, {
+    name: "Christina",
+    photo: "https://goo.gl/images/2XqE2z",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "1",
+      "2"
+    ]
+  }, {
+    name: "Hidden Beaute",
+    photo: "https://cdn.pixabay.com/photo/2018/01/06/09/25/hijab-3064633_1280.jpg",
+    scores: [
+      "2",
+      "4",
+      "3",
+      "4",
+      "2",
+      "3",
+      "3",
+      "3",
+      "4",
+      "3"
+    ]
+  }, {
+    name: "Dan",
+    photo: "https://www.google.com/search?q=man&safe=active&source=lnms&tbm=isch&sa=X&ved=0a" +
+        "hUKEwjggtTYwsfbAhVGy1QKHY8KBGMQ_AUICigB&biw=1230&bih=573#imgrc=E_KQXHj3lJpuwM:",
+    scores: [
+      "2",
+      "3",
+      "3",
+      "3",
+      "2",
+      "4",
+      "3",
+      "2",
+      "3",
+      "3"
+    ]
+  }, {
+    name: "Dan",
+    photo: "https://www.google.com/search?q=man&safe=active&source=lnms&tbm=isch&sa=X&ved=0a" +
+        "hUKEwjggtTYwsfbAhVGy1QKHY8KBGMQ_AUICigB&biw=1230&bih=573#imgrc=E_KQXHj3lJpuwM:",
+    scores: [
+      "2",
+      "3",
+      "3",
+      "3",
+      "2",
+      "4",
+      "3",
+      "2",
+      "3",
+      "3"
+    ]
+  }, {
+    name: "Dan",
+    photo: "https://www.google.com/search?q=man&safe=active&source=lnms&tbm=isch&sa=X&ved=0a" +
+        "hUKEwjggtTYwsfbAhVGy1QKHY8KBGMQ_AUICigB&biw=1230&bih=573#imgrc=E_KQXHj3lJpuwM:",
+    scores: [
+      "2",
+      "3",
+      "3",
+      "3",
+      "2",
+      "4",
+      "3",
+      "2",
+      "3",
+      "3"
+    ]
+  }, {
+    name: "Mark Diong",
+    photo: "https://cdn.pixabay.com/photo/2017/10/30/19/41/puma-2903312_1280.jpg",
+    scores: [
+      "1",
+      "1",
+      "2",
+      "1",
+      "1",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  }, {
+    name: "Tea Earth",
+    photo: "https://cdn.pixabay.com/photo/2016/10/07/08/56/beauty-1721060_1280.jpg",
+    scores: [
+      "2",
+      "2",
+      "2",
+      "1",
+      "3",
+      "3",
+      "2",
+      "3",
+      "4",
+      "1"
+    ]
+  }, {
+    name: "dan",
+    photo: "https://www.google.com",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  }, {
+    name: "Grr Hulk",
+    photo: "https://cdn.pixabay.com/photo/2014/12/23/07/40/hulk-578088_1280.jpg",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "2",
+      "1",
+      "2",
+      "5",
+      "4",
+      "2"
+    ]
+  }, {
+    name: "dan",
+    photo: "https://www.google.com",
+    scores: [
+      "2",
+      "2",
+      "2",
+      "3",
+      "2",
+      "3",
+      "3",
+      "3",
+      "1",
+      "1"
+    ]
+  }, {
+    name: "Mike",
+    photo: "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg",
+    scores: [
+      "4",
+      "1",
+      "2",
+      "4",
+      "3",
+      "2",
+      "3",
+      "1",
+      "3",
+      "2"
+    ]
+  }, {
+    name: "Mike",
+    photo: "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg",
+    scores: [
+      "4",
+      "1",
+      "2",
+      "4",
+      "3",
+      "2",
+      "3",
+      "1",
+      "3",
+      "2"
+    ]
+  }, {
+    name: "Mike",
+    photo: "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg",
+    scores: [
+      "4",
+      "1",
+      "2",
+      "4",
+      "3",
+      "2",
+      "3",
+      "1",
+      "3",
+      "2"
+    ]
+  }, {
+    name: "Mike",
+    photo: "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg",
+    scores: [
+      "4",
+      "1",
+      "2",
+      "4",
+      "3",
+      "2",
+      "3",
+      "1",
+      "3",
+      "2"
+    ]
+  }, {
+    name: "Gabrielle Union",
+    photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2…YqSsMrbAhXvp1kKHfPrCwIQMwhDKA" +
+        "MwAw&iact=mrc&uact=8",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "4",
+      "4",
+      "1",
+      "3",
+      "1",
+      "3",
+      "4"
+    ]
+  }, {
+    name: "Stanley Louis",
+    photo: "https://cdn.pixabay.com/photo/2018/06/08/23/35/black-panther-3463377_1280.jpg",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "2",
+      "3",
+      "3",
+      "3",
+      "3",
+      "4"
+    ]
+  }, {
+    name: "Mike",
+    photo: "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg",
+    scores: [
+      "4",
+      "1",
+      "2",
+      "4",
+      "3",
+      "2",
+      "3",
+      "1",
+      "3",
+      "2"
+    ]
+  }
+];
 
 module.exports = friends;
